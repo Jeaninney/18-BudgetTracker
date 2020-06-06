dabdff26ab3d919170e9ecececcd60af68a70d62
Jeaninne <jeaninnel@hotmail.com>
2020-06-06 13:50:54 -0400

    updated database for heroku deployment

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds011903.mlab.com:11903/heroku_d8gfx7vs";
+mongoose.connect(MONGODB_URI, { useMongoClient: true, useNewUrlParser: true, useFindAndModify: false });
 
 // routes
 app.use(require("./routes/api.js"));
