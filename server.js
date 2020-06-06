7b5dec6b8a40d861ffe04db692616fcd1964ebba
Jeaninne <jeaninnel@hotmail.com>
2020-06-06 14:10:03 -0400

    made small change to server.js for heroku deployment

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds011903.mlab.com:11903/heroku_d8gfx7vs";
-mongoose.connect(MONGODB_URI, { useMongoClient: true, useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false });
+
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
